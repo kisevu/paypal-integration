c56f5ebea94cbb967fd5d303ae0cfdacbf7e1cd3
ameda-kevin <amedakevin@gmail.com>
2024-02-29 16:32:26 +0300

    bootstrapped

---
+++ src/main/java/com/ameda/kevin/paypalintegration/PaypalIntegrationApplication.java
@@ -0,0 +1,10 @@
+package com.ameda.kevin.paypalintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PaypalIntegrationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ameda/kevin/paypalintegration/PaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameda.kevin.paypalintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
